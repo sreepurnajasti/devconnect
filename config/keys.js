15ff7ed498b86e0b9986a38fde7e450c28847518
sreepurnajasti <sreepurna.jasti@gmail.com>
2018-12-28 19:19:00 +0530

    use mongoose to connect to mongodb

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://admin:admin01@ds145194.mlab.com:45194/devconnect"
+};
--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const app = express();
-
-const users = require("./routes/api/users");
-const profile = require("./routes/api/profile");
-const posts = require("./routes/api/posts");
-
 const port = process.env.PORT || 5000;
-
 /* @route GET /
    @desc Test the basic route
    @access public
 */
 app.get("/", (req, res) => res.send("hello!"));
 
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+mongoose
+  .connect(db)
+  .then(() => console.log("momgodb connected"))
+  .catch(err => console.log(err));
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 //use routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);

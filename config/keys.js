926e0af2b2cd8c67d9ff45283d1d628e654a31b4
sreepurnajasti <sreepurna.jasti@gmail.com>
2019-01-01 18:03:31 +0530

    passport-jwt for protected route access - auth token gen

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://admin:admin01@ds145194.mlab.com:45194/devconnect"
+  mongoURI: "mongodb://admin:admin01@ds145194.mlab.com:45194/devconnect",
+  secretOrKey: "70726F64657820746563686E6F6C6F676965730A" //base16 encoder of prodex technologies
 };

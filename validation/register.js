ea422b46e50422549f9c2adbf95605efb75edf6a
sreepurnajasti <sreepurna.jasti@gmail.com>
2019-01-01 17:48:03 +0530

    register valiation & isEmpty function creation

---
+++ validation/register.js
@@ -0,0 +1,34 @@
+const validator = require("validator");
+const isEmpty = require("./isEmpty");
+
+module.exports = function validateRegisterInput(data) {
+  let errors = {};
+  data.name = isEmpty(data.name) ? data.name : "";
+  data.email = isEmpty(data.email) ? data.email : "";
+  data.password = isEmpty(data.password) ? data.password : "";
+
+  if (!validator.isLength(data.name, { min: 2, max: 30 })) {
+    errors.name = "Name must be between 2 and 30 characters";
+  }
+  if (!validator.isLength(data.email, { min: 2, max: 50 })) {
+    errors.email = "Email must be between 2 and 50 characters";
+  }
+  if (!validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = "Password must be between 6 and 30 characters";
+  }
+
+  if (validator.isEmpty(data.name)) {
+    errors.name = "Name field is required";
+  }
+  if (validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  }
+  if (validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
